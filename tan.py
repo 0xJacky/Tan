d66e4ad5050f4c5ca14c9d286524b74b54218150
0xJacky <jacky-943572677@qq.com>
2019-01-30 00:08:41 +0800

    v0.1
    1. Fixed the issue when two robots in a chatroom repeat with each other.
    2. Optimized the message form

--- tan.py
+++ tan.py
@@ -5,53 +5,82 @@ import itchat
 import time
 from db import Database
 db = Database()
-VERSION = '0.0.1'
+VERSION = '0.1'
 # 登陆
 itchat.auto_login(True)
+#获取学生名单
+def get_students_list():
+    sql = "SELECT * FROM `%s_students`" % db.sql_prefix
+    print(sql)
+    return db.fetchall(sql)
+#获取打卡列表
+def query_clockin_list():
+    date = time.strftime("%Y-%m-%d", time.localtime())
+    sql = "SELECT s.`id`, t.`id`, s.`name`, t.`date` FROM (%s_task as t INNER JOIN %s_log as l ON t.`ID` = l.`task_id`) \
+        INNER JOIN %s_students as s ON s.`ID` = l.`student_id` WHERE t.`date`='%s'" % (db.sql_prefix, db.sql_prefix, db.sql_prefix, date)
+    print(sql)
+    res = db.fetchall(sql)
+    message = '%s 打卡记录\n' % date
+    t = 0
+    for i in res:
+        t += 1
+        message += 'No.%s %s\n' % (t, i['name'])
+    return message[:-1]
+#打卡
+def handle_clockin(msg):
+    message = ''
+    sql = "SELECT * FROM `%s_students` WHERE `name`='%s'" % (db.sql_prefix, msg['ActualNickName'])
+    print(sql)
+    stu_res = db.fetchone(sql)
+
+    sql = "SELECT * FROM `%s_task` WHERE `date`='%s'" % (db.sql_prefix, time.strftime("%Y-%m-%d", time.localtime()))
+    print(sql)
+    task_res = db.fetchone(sql)
+    if (stu_res):
+        stu_id = stu_res['ID']
+        if (task_res):
+            task_id = task_res['ID']
+            print('已匹配 student_id:%s task_id:%s' % (stu_id, task_id))
+            sql = "SELECT * FROM `%s_log` WHERE `student_id`='%s' AND `task_id`='%s'" % (db.sql_prefix, stu_id, task_id)
+            print(sql)
+            link_res = db.fetchone(sql)
+            if not link_res:
+                sql = "INSERT INTO `%s_log` (student_id, task_id) VALUE ('%s', '%s')" % (db.sql_prefix, stu_id, task_id)
+                print(sql)
+                try:
+                    db.query(sql)
+                    message += '打卡成功'
+                except Exception as e:
+                    message += '打卡失败，错误信息: %s' % e
+            else:
+                message += '你已经打过卡了'
+        else:
+            print('已匹配 ID:%s, 今天没有打卡任务噢') % stu_id
+            message += '今天没有打卡任务噢' % stu_id
+
+    else:
+        message += '未匹配，请修改群名片'
+    return message
+
 # 处理群聊消息
 @itchat.msg_register(itchat.content.TEXT, isGroupChat = True)
 def text_reply(msg):
-    if msg['isAt']:
-        message = 'Project Tan V%s' % VERSION
-        if(u'打卡' in msg['Content']):
-            if (u'获取学生名单' in msg['Content']):
-                sql = "SELECT * FROM `%s_students`" % db.sql_prefix
-                message = db.fetchall(sql)
-        if(u'查询打卡' in msg['Content']):
-            sql = "SELECT s.`id`, t.`id`, s.`name` FROM (%s_task as t INNER JOIN %s_log as l ON t.`ID` = l.`task_id`) \
-    INNER JOIN %s_students as s ON s.`ID` = l.`student_id`" % ((db.sql_prefix,)*3)
-            print(sql)
-            res = db.fetchall(sql)
-            message +=str(res)
-        if(u'我' in msg['Content']):
-            sql = "SELECT * FROM `%s_students` WHERE `name`='%s'" % (db.sql_prefix, msg['ActualNickName'])
-            stu_res = db.fetchone(sql)
-
-            sql = "SELECT * FROM `%s_task` WHERE `date`='%s'" % (db.sql_prefix, time.strftime("%Y-%m-%d", time.localtime()))
-            print(sql)
-            task_res = db.fetchone(sql)
-            if(stu_res):
-                stu_id = stu_res['ID']
-                if (task_res):
-                    task_id = task_res['ID']
-                    message +='已匹配 student_id:%s task_id:%s' % (stu_id, task_id)
-                    sql = "SELECT * FROM `%s_log` WHERE `student_id`='%s' AND `task_id`='%s'" % (db.sql_prefix, stu_id, task_id)
-                    link_res = db.fetchone(sql)
-                    if not link_res:
-                        sql = "INSERT INTO `%s_log` (student_id, task_id) VALUE ('%s', '%s')" % (db.sql_prefix, stu_id, task_id)
-                        try:
-                            db.query(sql)
-                            message += '打卡成功'
-                        except Exception as e:
-                            message += '打卡失败，错误信息: %s' % e
-                    else:
-                        message += '你已经打过卡了'
-                else:
-                    message += '已匹配 ID:%s, 今天没有打卡任务噢' % stu_id
-
-            else:
-                message += '未匹配，请修改群名片'
-
-        itchat.send(u'@%s\u2005I received: %s, %s' % (msg['ActualNickName'], msg['Content'], message), msg['FromUserName'])
+    print('当前消息:'+msg['Content'])
+    if u'打卡' in msg['Content'] and 'Tan' not in msg['Content']:
+        print('已匹配到关键词'+'打卡')
+        message = 'Project Tan v%s\n' % VERSION
+        if (u'获取学生名单' in msg['Content']):
+            message += str(get_students_list())
+        elif(u'查询打卡' in msg['Content']):
+            print('已匹配到关键词' + '查询')
+            message +=str(query_clockin_list())
+        elif(u'我' in msg['Content']):
+            print('已匹配到关键词' + '我')
+            message += handle_clockin(msg)
+            print(message)
+        else:
+            message += '无法理解'
+        print(message)
+        itchat.send(u'@%s\u2005%s' % (msg['ActualNickName'], message), msg['FromUserName'])
 
 itchat.run()
